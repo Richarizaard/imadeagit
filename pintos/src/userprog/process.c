fc4fd42794be2015c949b71a55befdbf4539e9a7
Samuel Turner-Lill <ssl@viper.cs.ksu.edu>
2017-02-28 20:13:39 -0600

    -Changed some pants

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -82,12 +82,13 @@ start_process (void *file_name_)
    child of the calling process, or if process_wait() has already
    been successfully called for the given TID, returns -1
    immediately, without waiting.
-
+   
    This function will be implemented in problem 2-2.  For now, it
    does nothing. */
 int
 process_wait (tid_t child_tid UNUSED) 
 {
+	while(true);
   return -1;
 }
 
@@ -437,7 +438,7 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE;
+        *esp = PHYS_BASE - 12;
       else
         palloc_free_page (kpage);
     }
