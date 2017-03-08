c0e04891fe13a50ae533c14fb0e87c62061e8419
Michael Johnson <michaeljohnson@ksu.edu>
2017-03-07 18:08:04 -0600

    Add check for valid user pointer

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -1,8 +1,10 @@
 #include "userprog/syscall.h"
+#include "userprog/pagedir.h"
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -17,4 +19,19 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+}
+
+/*
+  Checks if the pointer is not a null pointer, not a pointer to unmapped virtual memory, and not a pointer to kernel virtual address space
+*/
+static bool
+check_user_pointer_validity(uint32_t *pd, const void * ptr)
+{
+  if (ptr == NULL)
+    return false;
+  if (is_kernel_vaddr(ptr))
+    return false;
+  if (pagedir_get_page(pd, ptr) == NULL)
+    return false;
+  return true;
 }
