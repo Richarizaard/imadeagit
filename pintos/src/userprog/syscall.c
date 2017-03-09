6cc49d4f99866e48f566893fd13be36c4550927a
Michael Johnson <michaeljohnson@ksu.edu>
2017-03-09 17:35:12 -0600

    Seperate syscall routing to seperate function

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -11,6 +11,7 @@ static void syscall_handler (struct intr_frame *);
 static void syscall_halt();
 static void syscall_exit();
 static void syscall_write(void * arg1);
+static void route_syscall(syscall_nums num, void * arg_start);
 static bool check_user_pointer_validity(uint32_t *pd, const void * ptr);
 
 void
@@ -31,56 +32,8 @@ syscall_handler (struct intr_frame *f)
 
   printf("system call! sysnum: %d pointer: %p\n", syscall_num, f->esp);
 
-  switch (syscall_num)
-  {
-    case SYS_HALT:
-		syscall_halt();
-		break;
-	case SYS_EXIT:
-		syscall_exit();
-		break;
-	case SYS_EXEC:
-		break;
-	case SYS_WAIT:
-		break;
-	case SYS_CREATE:
-		break;
-	case SYS_REMOVE:
-		break;
-	case SYS_OPEN:
-		break;
-	case SYS_FILESIZE:
-		break;
-	case SYS_READ:
-		break;
-	case SYS_WRITE:
-		syscall_write(arg1);
-		break;
-	case SYS_SEEK:
-		break;
-	case SYS_TELL:
-		break;
-	case SYS_CLOSE:
-		break;
+  route_syscall(syscall_num, arg1);
 
-	case SYS_MMAP:
-		break;
-	case SYS_MUNMAP:
-		break;
-
-	case SYS_CHDIR:
-		break;
-	case SYS_MKDIR:
-		break;
-	case SYS_READDIR:
-		break;
-	case SYS_ISDIR:
-		break;
-	case SYS_INUMBER:
-		break;
-  default:
-	  break;
-  }
   thread_exit ();
 }
 
@@ -105,14 +58,68 @@ static void syscall_exit()
 */
 static void syscall_write(void * arg_start)
 {
-	int * arg1 = (int *)arg_start;
-	void ** arg2 = (void **)((int *)arg1 + 1);
-	int * arg3 = (int *)((void *)arg2 + 1);
+  int * arg1 = (int *)arg_start;
+  void ** arg2 = (void **)((int *)arg1 + 1);
+  int * arg3 = (int *)((void *)arg2 + 1);
 
-	int handle = *arg1;
-	void *buffer = *arg2;
-	unsigned length = *arg3;
-	printf("%s", buffer);
+  int handle = *arg1;
+  void *buffer = *arg2;
+  unsigned length = *arg3;
+  printf("%s", buffer);
+}
+
+static void route_syscall(syscall_nums num, void * arg_start)
+{
+  switch (num)
+  {
+  case SYS_HALT:
+    syscall_halt();
+    break;
+  case SYS_EXIT:
+    syscall_exit();
+    break;
+  case SYS_EXEC:
+    break;
+  case SYS_WAIT:
+    break;
+  case SYS_CREATE:
+    break;
+  case SYS_REMOVE:
+    break;
+  case SYS_OPEN:
+    break;
+  case SYS_FILESIZE:
+    break;
+  case SYS_READ:
+    break;
+  case SYS_WRITE:
+    syscall_write(arg_start);
+    break;
+  case SYS_SEEK:
+    break;
+  case SYS_TELL:
+    break;
+  case SYS_CLOSE:
+    break;
+
+  case SYS_MMAP:
+    break;
+  case SYS_MUNMAP:
+    break;
+
+  case SYS_CHDIR:
+    break;
+  case SYS_MKDIR:
+    break;
+  case SYS_READDIR:
+    break;
+  case SYS_ISDIR:
+    break;
+  case SYS_INUMBER:
+    break;
+  default:
+    break;
+  }
 }
 
 /*
