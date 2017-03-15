9be47f44315c7e8067206d5a110115a9a8ee2cc6
Michael Johnson <michaeljohnson@ksu.edu>
2017-03-14 23:38:08 -0500

    Update arst

--- pintos/src/examples/arst.c
+++ pintos/src/examples/arst.c
@@ -11,7 +11,6 @@ main (int argc, char **argv)
 	  return EXIT_SUCCESS;
 
   char * name = argv[1];
-  char * name2 = argv[2];
 
   bool creat = create(name, 29);
   if (creat)
@@ -20,9 +19,7 @@ main (int argc, char **argv)
 	  printf("Noh cree ate sucess...\n");
 
   int fd1 = open(name);
-  int fd2 = open(name2);
   printf("Open returned %d.\n", fd1);
-  printf("Open returned %d.\n", fd2);
   
   printf("Writing 'Interior crocodile alligator' to file. %d bytes.\n", write(fd1, "Interior crocodile alligator", 28));
   
@@ -34,20 +31,20 @@ main (int argc, char **argv)
   write(fd1, "Chevrolet", 9);
 
   close(fd1);
-  close(fd2);
   
   printf("Filezize returned %d.\n", filesize(fd1));
 
   int fd3 = open(name);
-  int fd4 = open(name2);
+  int fd4 = open(name);
   
   printf("Open returned %d.\n", fd3);
   printf("Open returned %d.\n", fd4);
   
+  remove(fd4);
+  printf("Remove was a %d\n", remove(name));
+  
   char buffGuy[28];
   int readCount = read(fd3, buffGuy, 28);
   printf("Reading from file :: %d bytes read %s\n", readCount, buffGuy);
-
-  printf("Remove was a %d\n", remove(name));
   return EXIT_SUCCESS;
 }
